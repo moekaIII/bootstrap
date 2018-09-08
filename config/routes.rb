23cf5a610ba4392b5e3bb3e37329db94a5ec5ef6
Moeka Ihara <ihara.moeka@gmail.com>
2018-09-08 06:10:00 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get '/contacts', to: 'contacts#new'
+  get '/blogs', to: 'blogs#index'
+  get '/stocks', to: 'stocks#index'
+  
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  
+
+ resources :contacts
+end
---
+++ test/controllers/bligs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BligsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
