beec020833d9b39012a1a5d6784aa06400e0daff
Moeka Ihara <ihara.moeka@gmail.com>
2018-09-16 03:16:58 +0000

    bundle update

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Sample
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.web_console.whiny_requests = false
     config.web_console.whitelisted_ips = '192.168.0.0/16'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
