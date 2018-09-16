2bb6fccf619830dd57581d46815bfe1e45c5cd30
Moeka Ihara <ihara.moeka@gmail.com>
2018-09-16 05:16:32 +0000

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/contacts', to: 'contacts#new'
   get '/blogs', to: 'blogs#index'
   get '/stocks', to: 'stocks#index'
-  
   root 'static_pages/index'
   
   resources :blogs do
