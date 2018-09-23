c74b2926e43c7a58f491b14f7fe579f5968531c5
ihara moeka <ihara.moeka@gmail.com>
2018-09-23 19:57:55 +0900

    edit routes,rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/contacts', to: 'contacts#new'
   get '/blogs', to: 'blogs#index'
+  get '/stocks', to: 'stocks#index'
 
   resources :blogs do
     collection do
