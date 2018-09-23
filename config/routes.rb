2dcb3e8510a98d71cf2fc3df3d4b6077a25f7f7c
ihara moeka <ihara.moeka@gmail.com>
2018-09-23 19:39:31 +0900

    migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/contacts', to: 'contacts#new'
   get '/blogs', to: 'blogs#index'
-  get '/stocks', to: 'stocks#index'
 
   resources :blogs do
     collection do
