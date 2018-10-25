37115692e086f688777a3da6217a040ba6f315f3
ihara moeka <ihara.moeka@gmail.com>
2018-10-25 16:02:04 +0900

    Added image upload function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018022909) do
+ActiveRecord::Schema.define(version: 20181025062747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20181018022909) do
     t.string "title"
     t.text "content"
     t.bigint "user_id"
+    t.string "picture"
+    t.string "image"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
@@ -35,12 +37,19 @@ ActiveRecord::Schema.define(version: 20181018022909) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
